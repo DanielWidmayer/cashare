8c8281eacd6f09c20a401fe10c2086f75bd24cc5
Deny-bit <denis.alber@live.de>
2020-04-09 17:17:36 +0200

    mysql connection in server.js hizugefügt sowie ein insert an die datenbank, in register.html ein post zum server und validierung für passwort und email hinzugefügt

--- frontend_template/server.js
+++ frontend_template/server.js
@@ -2,6 +2,56 @@ var express = require('express');
 var bodyParser = require('body-parser');    // The body-parser is a type of middleware that allows you to handle requests more easily as it can wrap the whole requests body 
 var router = express.Router();
 var app = express();
+var mysql = require('mysql');
+
+// Connect to database
+// Node module mysql muss mit "npm install mysql" installiert werden, wenn noch nicht vorhanden..
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  // Port muss evtl. bei euch angepasst werden..
+  port: '3306',
+  // Datenbank "cashare" vor dem starten angelegt werden mit "CREATE DATABASE cashare;"
+  database: 'cashare'
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected to database!");
+  var sql = "SELECT 1 FROM user LIMIT 1;"
+  con.query(sql, function (err, result) {
+    if (err)
+    {
+      console.log(err);
+      console.log("No Database user.."); 
+      console.log("create databaase..");
+      var sql = "create table user("
+        +"userid int(9) not null auto_increment primary key," 
+        +"firstName varchar(255) not null,"
+        +"lastName varchar(255) not null,"
+        +"email VARCHAR(255),"
+        +"password varchar(255) not null,"
+        +"balance decimal(10,2) not null default('0')"
+        +");"
+
+      con.query(sql, function(err, result){
+          if(err) 
+          {
+            console.log("cannot create database...");
+          }
+          else
+          {
+          console.log("database user created!");// hier weiter machen
+          }
+      });
+    }
+    else{
+      console.log("database user allready exists!");
+    }
+  });
+});
+
 
 // static folder where static files like html are stored
 app.use(express.static('public'));
@@ -37,6 +87,14 @@ router.get('/register', function (req, res) {
   res.render('register.html');
 });
 
+router.post('/register', function(req, res){
+  res.status(200).json({'success': "User data reached!"});
+  console.log("The following data has been received:");
+  console.log(req.body);
+  console.log("Insert data to table user...");
+  createUser(req.body.firstName, req.body.lastName, req.body.eMail, req.body.password)
+});
+
 router.get('/tables', function(req, res) {
   res.render('tables.html');
 });
@@ -61,3 +119,19 @@ app.use(function(err, req, res, next) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+function createUser(firstName, lastName, eMail, password)
+{
+  var sql = `INSERT INTO user (firstName, lastName, eMail, password) VALUES ('${firstName}','${lastName}','${eMail}','${password}');`;
+  con.query(sql, function(err, result){
+    if(err)
+    {
+      console.log("There was a problem by adding a new user..");
+      console.log(err);
+    }
+    else{
+      console.log("Added user successfull!");
+      console.log(result);
+    }
+  })
+};
