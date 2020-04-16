63f06fb13b9a3369e1cb9538bdca081250854f6f
Daniel <qwerdani99@gmail.com>
2020-04-16 11:26:00 +0200

    expenses first version

--- js/demo/chart-bar-demo.js
+++ js/demo/chart-bar-demo.js
@@ -1,5 +1,5 @@
 // Set new default font family and font color to mimic Bootstrap's default styling
-Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+(Chart.defaults.global.defaultFontFamily = 'Nunito'), '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
 
 function number_format(number, decimals, dec_point, thousands_sep) {
@@ -8,10 +8,10 @@ function number_format(number, decimals, dec_point, thousands_sep) {
   number = (number + '').replace(',', '').replace(' ', '');
   var n = !isFinite(+number) ? 0 : +number,
     prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
-    sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
-    dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+    sep = typeof thousands_sep === 'undefined' ? ',' : thousands_sep,
+    dec = typeof dec_point === 'undefined' ? '.' : dec_point,
     s = '',
-    toFixedFix = function(n, prec) {
+    toFixedFix = function (n, prec) {
       var k = Math.pow(10, prec);
       return '' + Math.round(n * k) / k;
     };
@@ -28,18 +28,20 @@ function number_format(number, decimals, dec_point, thousands_sep) {
 }
 
 // Bar Chart Example
-var ctx = document.getElementById("myBarChart");
+var ctx = document.getElementById('myBarChart');
 var myBarChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
-    datasets: [{
-      label: "Revenue",
-      backgroundColor: "#4e73df",
-      hoverBackgroundColor: "#2e59d9",
-      borderColor: "#4e73df",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
-    }],
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    datasets: [
+      {
+        label: 'Revenue',
+        backgroundColor: '#3493FF',
+        hoverBackgroundColor: '#2e59d9',
+        borderColor: '#4e73df',
+        data: [9821, 5312, 7841, 4215, 6251, 14984, 5386, 8643, 6431, 1236, 4538, 8254],
+      },
+    ],
   },
   options: {
     maintainAspectRatio: false,
@@ -48,52 +50,56 @@ var myBarChart = new Chart(ctx, {
         left: 10,
         right: 25,
         top: 25,
-        bottom: 0
-      }
+        bottom: 0,
+      },
     },
     scales: {
-      xAxes: [{
-        time: {
-          unit: 'month'
+      xAxes: [
+        {
+          time: {
+            unit: 'month',
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false,
+          },
+          ticks: {
+            maxTicksLimit: 12,
+          },
+          maxBarThickness: 25,
         },
-        gridLines: {
-          display: false,
-          drawBorder: false
+      ],
+      yAxes: [
+        {
+          ticks: {
+            min: 0,
+            max: 15000,
+            maxTicksLimit: 5,
+            padding: 10,
+            // Include a dollar sign in the ticks
+            callback: function (value, index, values) {
+              return '$' + number_format(value);
+            },
+          },
+          gridLines: {
+            color: 'rgb(234, 236, 244)',
+            zeroLineColor: 'rgb(234, 236, 244)',
+            drawBorder: false,
+            borderDash: [2],
+            zeroLineBorderDash: [2],
+          },
         },
-        ticks: {
-          maxTicksLimit: 6
-        },
-        maxBarThickness: 25,
-      }],
-      yAxes: [{
-        ticks: {
-          min: 0,
-          max: 15000,
-          maxTicksLimit: 5,
-          padding: 10,
-          // Include a dollar sign in the ticks
-          callback: function(value, index, values) {
-            return '$' + number_format(value);
-          }
-        },
-        gridLines: {
-          color: "rgb(234, 236, 244)",
-          zeroLineColor: "rgb(234, 236, 244)",
-          drawBorder: false,
-          borderDash: [2],
-          zeroLineBorderDash: [2]
-        }
-      }],
+      ],
     },
     legend: {
-      display: false
+      display: false,
     },
     tooltips: {
       titleMarginBottom: 10,
       titleFontColor: '#6e707e',
       titleFontSize: 14,
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
+      backgroundColor: 'rgb(255,255,255)',
+      bodyFontColor: '#858796',
       borderColor: '#dddfeb',
       borderWidth: 1,
       xPadding: 15,
@@ -101,11 +107,11 @@ var myBarChart = new Chart(ctx, {
       displayColors: false,
       caretPadding: 10,
       callbacks: {
-        label: function(tooltipItem, chart) {
+        label: function (tooltipItem, chart) {
           var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
           return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
-        }
-      }
+        },
+      },
     },
-  }
+  },
 });
