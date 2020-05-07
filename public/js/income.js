0e5ac366fb0e76b5f21cd2b8f05b094cd7a04c82
Denis Alber <alber.denis@gmai.com>
2020-05-07 12:44:10 +0200

    Update incom.html and expenses.html (input validation)

--- public/js/income.js
+++ public/js/income.js
@@ -1,7 +1,54 @@
+$("#transactionValue").on("keypress keyup blur", function (event) {
+    $(this).val($(this).val().replace(/[^0-9\.|\,]/g,''));
+    if(event.which == 44)
+    {
+    return true;
+    }
+    if ((event.which != 46 || $(this).val().indexOf('.') != -1) && (event.which < 48 || event.which > 57  )) {
+    
+      event.preventDefault();
+    }
+});
+
+$("#transactionValueModal").on("keypress keyup blur", function (event) {
+    $(this).val($(this).val().replace(/[^0-9\.|\,]/g,''));
+    if(event.which == 44)
+    {
+    return true;
+    }
+    if ((event.which != 46 || $(this).val().indexOf('.') != -1) && (event.which < 48 || event.which > 57  )) {
+    
+      event.preventDefault();
+    }
+});
+
 $("#addIncome").click(function(){
     var transactionValue = ($("#transactionValue").val()).replace(',', '.');
     var timePeriod = $("#timePeriod").val();
     var chooseCategory = $("#chooseCategory").val();
+
+    // temporär bis einheitliches Fehlermanagment vorhanden ist..
+    if(chooseCategory == "Choose Category" || timePeriod == "Choose Time period" || transactionValue.charAt(0) == '.' || transactionValue == "")
+    {
+        if(transactionValue.charAt(0) == '.' || transactionValue == "")
+        {
+            $("#transactionValue").css("box-shadow", "0 0 0 3px rgba(255, 0, 0, 0.5)");
+            $('#transactionValue').css('border-color', 'red');
+        }
+        if(chooseCategory == "Choose Category")
+        {
+            $("#chooseCategory").css("box-shadow", "0 0 0 3px rgba(255, 0, 0, 0.5)");
+            $('#chooseCategory').css('border-color', 'red');
+        }
+        if(timePeriod == "Choose Time period")
+        {
+            $("#timePeriod").css("box-shadow", "0 0 0 3px rgba(255, 0, 0, 0.5)");
+            $('#timePeriod').css('border-color', 'red');
+        }
+        return false;
+    }
+
+
 
     var transaction = {'transactionValue':transactionValue, 'timePeriod':timePeriod,'chooseCategory':chooseCategory};
     $.post( "/income", transaction ) 
@@ -15,6 +62,27 @@ $("#addIncomeModal").click(function(){
     var transactionValue = ($("#transactionValueModal").val()).replace(',', '.');
     var timePeriod = $("#timePeriodModal").val();
     var chooseCategory = $("#chooseCategoryModal").val();
+
+        // temporär bis einheitliches Fehlermanagment vorhanden ist..
+        if(chooseCategory == "Choose Category" || timePeriod == "Choose Time period" || transactionValue.charAt(0) == '.' || transactionValue == "")
+        {
+            if(transactionValue.charAt(0) == '.' || transactionValue == "")
+            {
+                $("#transactionValueModal").css("box-shadow", "0 0 0 3px rgba(255, 0, 0, 0.5)");
+                $('#transactionValueModal').css('border-color', 'red');
+            }
+            if(chooseCategory == "Choose Category")
+            {
+                $("#chooseCategoryModal").css("box-shadow", "0 0 0 3px rgba(255, 0, 0, 0.5)");
+                $('#chooseCategoryModal').css('border-color', 'red');
+            }
+            if(timePeriod == "Choose Time period")
+            {
+                $("#timePeriodModal").css("box-shadow", "0 0 0 3px rgba(255, 0, 0, 0.5)");
+                $('#timePeriodModal').css('border-color', 'red');
+            }
+            return false;
+        }
 
     var transaction = {'transactionValue':transactionValue, 'timePeriod':timePeriod,'chooseCategory':chooseCategory};
     $.post( "/income", transaction ) 
