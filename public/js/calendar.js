a351a22498545a15ed64cd8c6f5fe15e85ad215f
Daniel <57903719+DanielWidmayer@users.noreply.github.com>
2020-05-08 11:34:27 +0200

    Calendar + Time Picker

---
+++ public/js/calendar.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('#datetimepicker').datetimepicker({
+        icons: {
+            time: "fa fa-clock text-primary",
+            date: "fa fa-calendar text-primary",
+            up: "fa fa-arrow-up text-primary",
+            down: "fa fa-arrow-down text-primary"
+        }
+    });
+});
