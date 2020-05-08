fa5a6eb168767eb3c1dd08bf400bd06dafc52231
JBHRgm <62023324+JBHRgm@users.noreply.github.com>
2020-05-08 10:23:07 +0200

    login flash

--- server.js
+++ server.js
@@ -87,200 +87,201 @@ const isAuthenticated = function (req, res, next) {
       }
       return res.redirect('/login');
     }
+  }
+}
+app.use('/', router);
 
-    app.use('/', router);
+// Base Page
+router.get('/', function (req, res) {
+  return res.render('home.html');
+});
 
-    // Base Page
-    router.get('/', function (req, res) {
-      return res.render('home.html');
-    });
+// Home - Cashboard
+router.get('/home', isAuthenticated, async function (req, res) {
+  return res.render('index.html', { username: [req.user[1], req.user[2]], usermail: req.user[3], userphone: req.user[4], userbalance: req.user[5], userpic: req.user[6], pagename: 'index' });
+});
 
-    // Home - Cashboard
-    router.get('/home', isAuthenticated, async function (req, res) {
-      return res.render('index.html', { username: [req.user[1], req.user[2]], usermail: req.user[3], userphone: req.user[4], userbalance: req.user[5], userpic: req.user[6], pagename: 'index' });
-    });
+router.get('/blank', isAuthenticated, async function (req, res) {
+  return res.render('blank.html', { username: [req.user[1], req.user[2]], usermail: req.user[3], userphone: req.user[4], userbalance: req.user[5], userpic: req.user[6], pagename: 'index' });
+});
 
-    router.get('/blank', isAuthenticated, async function (req, res) {
-      return res.render('blank.html', { username: [req.user[1], req.user[2]], usermail: req.user[3], userphone: req.user[4], userbalance: req.user[5], userpic: req.user[6], pagename: 'index' });
-    });
+// Login
+router.get('/login', function (req, res) {
+  if (req.user) return res.redirect('/home');
+  return res.render('login.html', { errflash: req.flash('error') });
+});
 
-    // Login
-    router.get('/login', function (req, res) {
-      if (req.user) return res.redirect('/home');
-      return res.render('login.html', { errflash: req.flash('error') });
-    });
+router.post('/login', passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }), function (req, res) {
+  return res.redirect('/home');
+});
 
-    router.post('/login', passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }), function (req, res) {
+
+router.get('/logout', isAuthenticated, (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.log(err);
       return res.redirect('/home');
-    });
+    }
+    res.clearCookie(process.env.SESS_NAME);
+    req.logout();
+    return res.redirect('/login');
+  });
+});
+
+router.get('/register', function (req, res) {
+  return res.render('register.html');
+});
+
+router.post('/register', async function (req, res) {
+  //res.status(200).json({'success': "User data reached!"});
+  console.log('The following data has been received:');
+  console.log(req.body);
+  try {
+    user_res = await dbsql.db_user.registerUser(req.body.firstName, req.body.lastName, req.body.eMail, req.body.password);
+    return res.redirect('/login');
+  } catch (err) {
+    console.log(err);
+    return res.redirect('/register');
+  }
+});
+
+router.get('/tables', isAuthenticated, async function (req, res) {
+  return res.render('logs-tables.html', { username: [req.user[1], req.user[2]], usermail: req.user[3], userphone: req.user[4], userbalance: req.user[5], userpic: req.user[6], pagename: 'logs' });
+});
 
 
-    router.get('/logout', isAuthenticated, (req, res) => {
-      req.session.destroy((err) => {
-        if (err) {
-          console.log(err);
-          return res.redirect('/home');
-        }
-        res.clearCookie(process.env.SESS_NAME);
-        req.logout();
-        return res.redirect('/login');
-      });
-    });
+router.get('/groups', isAuthenticated, async function (req, res) {
+  return res.render('payment-groups.html', { username: [req.user[1], req.user[2]], usermail: req.user[3], userphone: req.user[4], userbalance: req.user[5], userpic: req.user[6], pagename: 'groups' });
+});
 
-    router.get('/register', function (req, res) {
-      return res.render('register.html');
-    });
+// Income
+router.get('/income', isAuthenticated, async function (req, res) {
+  try {
+    var q_cat = await dbsql.db_cat.getCategorysByUserID(req.user[0], 0);
+    var q_trans = await dbsql.db_trans.getTransactionsByUserID(req.user[0], 0);
+  } catch (err) {
+    console.log(err);
+  }
+  return res.render('income.html', { username: [req.user[1], req.user[2]], usermail: req.user[3], userphone: req.user[4], userbalance: req.user[5], userpic: req.user[6], pagename: 'income', categorys: q_cat, transactions: q_trans });
+});
 
-    router.post('/register', async function (req, res) {
-      //res.status(200).json({'success': "User data reached!"});
-      console.log('The following data has been received:');
-      console.log(req.body);
-      try {
-        user_res = await dbsql.db_user.registerUser(req.body.firstName, req.body.lastName, req.body.eMail, req.body.password);
-        return res.redirect('/login');
-      } catch (err) {
-        console.log(err);
-        return res.redirect('/register');
+router.post('/income', isAuthenticated, async function (req, res) {
+  try {
+    let sqlret = await dbsql.db_trans.insertTransaction(req.body.transactionValue, req.body.timePeriod, req.body.chooseCategory, 0, req.user[0]);
+    res.send(req.user[1] + " " + sqlret);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.post('/addCategory', isAuthenticated, async function (req, res) {
+  try {
+    console.log("Category in Datenbank schreiben..");
+    console.log(req.body);
+    let ret = await dbsql.db_cat.addCategory(req.body.newCategory, req.body.description, req.body.category_isExpense, req.user[0]);
+
+    console.log("Category auslesen..");
+    let q_cat = await dbsql.db_cat.getCategoryByCatNameAndUserID(req.user[0], req.body.newCategory, req.body.category_isExpense);
+    var q_categorys = JSON.parse(JSON.stringify(q_cat));
+
+    console.log(q_categorys);
+    res.send(q_categorys);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get('/groups/certaingroup', isAuthenticated, async function (req, res) {
+  res.render('paymentgroup-shareboard.html', { username: [req.user[1], req.user[2]], usermail: req.user[3], userphone: req.user[4], userbalance: req.user[5], userpic: req.user[6], pagename: 'profile' });
+});
+
+
+
+router.get('/profile', isAuthenticated, async function (req, res) {
+  return res.render('profile.html', { username: [req.user[1], req.user[2]], usermail: req.user[3], userphone: req.user[4], userpic: req.user[6], pagename: 'profile' });
+});
+
+router.post('/profile', isAuthenticated, upload.single('pic'), async function (req, res) {
+  try {
+    if (req.file) {
+      await dbsql.db_user.changeProfileImg(req.user[0]);
+    }
+    else {
+      await dbsql.db_user.changeNameAndMail(req.user[0], req.body.firstname, req.body.lastname, req.body.mail);
+      if (req.body.old_password && req.body.new_password) {
+        await dbsql.db_user.changePassword(req.user[0], req.body.old_password, req.body.new_password);
       }
-    });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+  let temp = await dbsql.db_user.getDataByID(req.user[0]);
+  req.login(temp, (err) => {
+    if (err) return next(err);
+    return res.redirect(req.originalUrl);
+  })
+});
 
-    router.get('/tables', isAuthenticated, async function (req, res) {
-      return res.render('logs-tables.html', { username: [req.user[1], req.user[2]], usermail: req.user[3], userphone: req.user[4], userbalance: req.user[5], userpic: req.user[6], pagename: 'logs' });
-    });
 
-
-    router.get('/groups', isAuthenticated, async function (req, res) {
-      return res.render('payment-groups.html', { username: [req.user[1], req.user[2]], usermail: req.user[3], userphone: req.user[4], userbalance: req.user[5], userpic: req.user[6], pagename: 'groups' });
-    });
-
-    // Income
-    router.get('/income', isAuthenticated, async function (req, res) {
-      try {
-        var q_cat = await dbsql.db_cat.getCategorysByUserID(req.user[0], 0);
-        var q_trans = await dbsql.db_trans.getTransactionsByUserID(req.user[0], 0);
-      } catch (err) {
-        console.log(err);
-      }
-      return res.render('income.html', { username: [req.user[1], req.user[2]], usermail: req.user[3], userphone: req.user[4], userbalance: req.user[5], userpic: req.user[6], pagename: 'income', categorys: q_cat, transactions: q_trans });
-    });
-
-    router.post('/income', isAuthenticated, async function (req, res) {
-      try {
-        let sqlret = await dbsql.db_trans.insertTransaction(req.body.transactionValue, req.body.timePeriod, req.body.chooseCategory, 0, req.user[0]);
-        res.send(req.user[1] + " " + sqlret);
-      } catch (err) {
-        console.log(err);
-      }
-    });
-
-    router.post('/addCategory', isAuthenticated, async function (req, res) {
-      try {
-        console.log("Category in Datenbank schreiben..");
-        console.log(req.body);
-        let ret = await dbsql.db_cat.addCategory(req.body.newCategory, req.body.description, req.body.category_isExpense, req.user[0]);
-
-        console.log("Category auslesen..");
-        let q_cat = await dbsql.db_cat.getCategoryByCatNameAndUserID(req.user[0], req.body.newCategory, req.body.category_isExpense);
-        var q_categorys = JSON.parse(JSON.stringify(q_cat));
-
-        console.log(q_categorys);
-        res.send(q_categorys);
-      } catch (err) {
-        console.log(err);
-      }
-    });
-
-    router.get('/groups/certaingroup', isAuthenticated, async function (req, res) {
-      res.render('paymentgroup-shareboard.html', { username: [req.user[1], req.user[2]], usermail: req.user[3], userphone: req.user[4], userbalance: req.user[5], userpic: req.user[6], pagename: 'profile' });
-    });
+router.get('/chat', isAuthenticated, async function (req, res) {
+  return res.render('chat.html', { username: [req.user[1], req.user[2]], usermail: req.user[3], userphone: req.user[4], userbalance: req.user[5], userpic: req.user[6], pagename: 'chat' });
+});
 
 
 
-    router.get('/profile', isAuthenticated, async function (req, res) {
-      return res.render('profile.html', { username: [req.user[1], req.user[2]], usermail: req.user[3], userphone: req.user[4], userpic: req.user[6], pagename: 'profile' });
-    });
+router.get('/expenses', isAuthenticated, async function (req, res) {
+  try {
+    var q_cat = await dbsql.db_cat.getCategorysByUserID(req.user[0], 1);
+    var q_trans = await dbsql.db_trans.getTransactionsByUserID(req.user[0], 1);
+  } catch (err) {
+    console.log(err);
+  }
+  return res.render('expenses.html', { username: [req.user[1], req.user[2]], usermail: req.user[3], userphone: req.user[4], userbalance: req.user[5], userpic: req.user[6], pagename: 'expenses', categorys: q_cat, transactions: q_trans });
+});
 
-    router.post('/profile', isAuthenticated, upload.single('pic'), async function (req, res) {
-      try {
-        if (req.file) {
-          await dbsql.db_user.changeProfileImg(req.user[0]);
-        }
-        else {
-          await dbsql.db_user.changeNameAndMail(req.user[0], req.body.firstname, req.body.lastname, req.body.mail);
-          if (req.body.old_password && req.body.new_password) {
-            await dbsql.db_user.changePassword(req.user[0], req.body.old_password, req.body.new_password);
-          }
-        }
-      } catch (err) {
-        console.log(err);
-      }
-      let temp = await dbsql.db_user.getDataByID(req.user[0]);
-      req.login(temp, (err) => {
-        if (err) return next(err);
-        return res.redirect(req.originalUrl);
-      })
-    });
+router.post('/expenses', isAuthenticated, async function (req, res) {
+  try {
+    console.log(req.body);
+    let sqlret = await dbsql.db_trans.insertTransaction(req.body.transactionValue, 1, req.body.chooseCategory, 1, req.user[0]);
+    res.send(req.user[1] + " " + sqlret);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
+router.get('/settings', isAuthenticated, async function (req, res) {
+  return res.render('blank.html', { username: [req.user[1], req.user[2]], usermail: req.user[3], userphone: req.user[4], userbalance: req.user[5], userpic: req.user[6], pagename: 'blank' });
+});
 
-    router.get('/chat', isAuthenticated, async function (req, res) {
-      return res.render('chat.html', { username: [req.user[1], req.user[2]], usermail: req.user[3], userphone: req.user[4], userbalance: req.user[5], userpic: req.user[6], pagename: 'chat' });
-    });
+router.get('/alerts', isAuthenticated, async function (req, res) {
+  return res.render('alerts.html', { username: [req.user[1], req.user[2]], usermail: req.user[3], userphone: req.user[4], userbalance: req.user[5], userpic: req.user[6], pagename: 'alerts' });
+});
 
+router.get('/try', isAuthenticated, function (req, res) {
+  throw new Error('this was a test, and you failed!');
+});
 
+app.use(function (req, res, next) {
+  res.setHeader('Cache-Control', 'no-cache, must-revalidate, no-store');
+  res.setHeader('Pragma', 'no-cache'); // HTTP 1.0.
+  res.setHeader('Expires', '0'); // Proxies.
+  next();
+});
 
-    router.get('/expenses', isAuthenticated, async function (req, res) {
-      try {
-        var q_cat = await dbsql.db_cat.getCategorysByUserID(req.user[0], 1);
-        var q_trans = await dbsql.db_trans.getTransactionsByUserID(req.user[0], 1);
-      } catch (err) {
-        console.log(err);
-      }
-      return res.render('expenses.html', { username: [req.user[1], req.user[2]], usermail: req.user[3], userphone: req.user[4], userbalance: req.user[5], userpic: req.user[6], pagename: 'expenses', categorys: q_cat, transactions: q_trans });
-    });
+// handle error 404 - page not found
+app.use('*', isAuthenticated, function (req, res, next) {
+  if (req.originalUrl != '/favicon.ico') {
+    return res.render('404.html', { username: [req.user[1], req.user[2]], usermail: req.user[3], userphone: req.user[4], userbalance: req.user[5], userpic: req.user[6], pagename: '404' });
+  }
+});
 
-    router.post('/expenses', isAuthenticated, async function (req, res) {
-      try {
-        console.log(req.body);
-        let sqlret = await dbsql.db_trans.insertTransaction(req.body.transactionValue, 1, req.body.chooseCategory, 1, req.user[0]);
-        res.send(req.user[1] + " " + sqlret);
-      } catch (err) {
-        console.log(err);
-      }
-    });
+// handle any server error
+app.use(function (err, req, res, next) {
+  console.log('Error handler');
+  console.error(err);
+  return res.status(500).send({ error: err.message });
+});
 
-    router.get('/settings', isAuthenticated, async function (req, res) {
-      return res.render('blank.html', { username: [req.user[1], req.user[2]], usermail: req.user[3], userphone: req.user[4], userbalance: req.user[5], userpic: req.user[6], pagename: 'blank' });
-    });
-
-    router.get('/alerts', isAuthenticated, async function (req, res) {
-      return res.render('alerts.html', { username: [req.user[1], req.user[2]], usermail: req.user[3], userphone: req.user[4], userbalance: req.user[5], userpic: req.user[6], pagename: 'alerts' });
-    });
-
-    router.get('/try', isAuthenticated, function (req, res) {
-      throw new Error('this was a test, and you failed!');
-    });
-
-    app.use(function (req, res, next) {
-      res.setHeader('Cache-Control', 'no-cache, must-revalidate, no-store');
-      res.setHeader('Pragma', 'no-cache'); // HTTP 1.0.
-      res.setHeader('Expires', '0'); // Proxies.
-      next();
-    });
-
-    // handle error 404 - page not found
-    app.use('*', isAuthenticated, function (req, res, next) {
-      if (req.originalUrl != '/favicon.ico') {
-        return res.render('404.html', { username: [req.user[1], req.user[2]], usermail: req.user[3], userphone: req.user[4], userbalance: req.user[5], userpic: req.user[6], pagename: '404' });
-      }
-    });
-
-    // handle any server error
-    app.use(function (err, req, res, next) {
-      console.log('Error handler');
-      console.error(err);
-      return res.status(500).send({ error: err.message });
-    });
-
-    app.listen(process.env.PORT, function () {
-      console.log(`Example app listening on port ${process.env.PORT}`);
-    });
+app.listen(process.env.PORT, function () {
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
