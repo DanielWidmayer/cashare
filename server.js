318370368e3d7f0a3c2a65bac057e1b02248f476
JBHRgm <62023324+JBHRgm@users.noreply.github.com>
2020-06-02 17:49:38 +0200

    flash middleware and other changes

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const dotenv = require('dotenv').config();
 // const util = require('util');
 const helmet = require('helmet');
 const express = require('express');
+const fav = require('serve-favicon');
+const path = require('path');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const router = express.Router();
 const passport = require('./modules/passport-config');
 //const initializePassPort = require('./modules/passport-config');
 const app = express();
-const flash = require('express-flash');
+const flash = require('connect-flash');
+const flash_mw = require('./modules/flashes');
 const dbsql = require('./dbsql');
 const upload = require('./modules/pic_upload');
 
@@ -18,7 +21,6 @@ if (dotenv.error) throw dotenv.error;
 const IN_PROD = process.env.NODE_ENV === 'production';
 const TTL = parseInt(process.env.SESS_LIFETIME);
 
-app.use(flash());
 // Helmet
 app.use(helmet());
 
@@ -42,11 +44,11 @@ app.use(
   })
 );
 
+// Serve Favicon
+app.use(fav(path.join(__dirname,'public','templates','favicon.ico')));
+
 // Database Connection
 dbsql.createConnection(process.env.DB_HOST, process.env.DB_USER, process.env.DB_PASSWORD, process.env.DB_PORT, process.env.DB_NAME);
-
-
-// TODO
 dbsql.connect();
 
 // static folder where static files like html are stored
@@ -56,8 +58,7 @@ app.set('views', __dirname + '/public/templates');
 // set the template engine to be ejs
 app.engine('html', require('ejs').renderFile);
 // usage of .html files
-//app.set('view engine', 'html');
-app.set('view engine', 'php');
+app.set('view engine', 'html');
 // support parsing of application/x-www-form-urlencoded post data
 app.use(
   express.urlencoded({
@@ -71,18 +72,27 @@ app.use(express.json());
 app.use(passport.initialize());
 app.use(passport.session());
 
-// auth middleware
+
+// -------------------------------------------------------------------------------------------------------------------------------- Middleware
+// flash middleware - allows to use req.flash()
+app.use(flash());
+app.use(flash_mw.flashMessage);
+
+// auth middleware  - will redirect user to /login if he tries to access a secure route while not being authenticated
 const isAuthenticated = function (req, res, next) {
-
   console.log("auth - " + req.originalUrl);
-
-  if (req.user && req.originalUrl != '/favicon.ico') {
-
-    return next();
-  }
-  return res.redirect('/login');
-
+  if (req.user) return next();
+  else return res.redirect('/login');
 }
+
+// home middleware - will redirect already authenticated users to /home if they try to access authentication routes like login or register
+const redirectHome = function (req, res, next) {
+  if(req.user) return res.redirect('/home');
+  else return next();
+}
+// -------------------------------------------------------------------------------------------------------------------------------- /Middleware
+
+// -------------------------------------------------------------------------------------------------------------------------------- Routes
 app.use('/', router);
 
 // Base Page
@@ -120,12 +130,12 @@ router.get('/blank', isAuthenticated, async function (req, res) {
 });
 
 // Login
-router.get('/login', function (req, res) {
+router.get('/login', redirectHome, function (req, res) {
   if (req.user) return res.redirect('/home');
-  return res.render('login.html', { errflash: req.flash('error') });
+  return res.render('login.html');
 });
 
-router.post('/login', passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }), function (req, res) {
+router.post('/login', redirectHome, passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }), function (req, res) {
   return res.redirect('/home');
 });
 
@@ -142,11 +152,12 @@ router.get('/logout', isAuthenticated, (req, res) => {
   });
 });
 
-router.get('/register', function (req, res) {
+router.get('/register', redirectHome, function (req, res) {
+
   return res.render('register.html', { errmsg: 0 });
 });
 
-router.post('/register', async function (req, res) {
+router.post('/register', redirectHome, async function (req, res) {
   //res.status(200).json({'success': "User data reached!"});
   console.log('The following data has been received:');
   console.log(req.body);
@@ -284,18 +295,17 @@ router.get('/try', isAuthenticated, function (req, res) {
   throw new Error('this was a test, and you failed!');
 });
 
-app.use(function (req, res, next) {
-  res.setHeader('Cache-Control', 'no-cache, must-revalidate, no-store');
-  res.setHeader('Pragma', 'no-cache'); // HTTP 1.0.
-  res.setHeader('Expires', '0'); // Proxies.
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.setHeader('Cache-Control', 'no-cache, must-revalidate, no-store');
+//   res.setHeader('Pragma', 'no-cache'); // HTTP 1.0.
+//   res.setHeader('Expires', '0'); // Proxies.
+//   next();
+// });
 
+// -------------------------------------------------------------------------------------------------------------------------------- Error handler
 // handle error 404 - page not found
 app.use('*', isAuthenticated, function (req, res, next) {
-  if (req.originalUrl != '/favicon.ico') {
-    return res.render('404.html', { username: [req.user[1], req.user[2]], usermail: req.user[3], userphone: req.user[4], userbalance: req.user[5], userpic: req.user[6], pagename: '404' });
-  }
+  return res.render('404.html', { username: [req.user[1], req.user[2]], usermail: req.user[3], userphone: req.user[4], userbalance: req.user[5], userpic: req.user[6], pagename: '404' });
 });
 
 // handle any server error
@@ -304,7 +314,9 @@ app.use(function (err, req, res, next) {
   console.error(err);
   return res.status(500).send({ error: err.message });
 });
+// -------------------------------------------------------------------------------------------------------------------------------- /Error handler
 
+// -------------------------------------------------------------------------------------------------------------------------------- listen
 app.listen(process.env.PORT, function () {
   console.log(`Example app listening on port ${process.env.PORT}`);
 });
